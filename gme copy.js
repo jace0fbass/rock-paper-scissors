5fedd346b8af85994af988dc664aee209c9871a8
Jason <jajones414@gmail.com>
2022-09-12 23:31:39 -0500

    I have made changes to the gme copy.js file

--- gme copy.js
+++ gme copy.js
@@ -9,7 +9,7 @@
 // deny them entry
 
 // define array to hold choices [r, p, s]
-var choices = [r, p, s]
+var choices = [r, p, s];
 // define var for wins
 var wins = 0;
 // define var for losses
@@ -21,50 +21,60 @@ var keepPlaying = true;
 
 while (keepPlaying) {
 }
+
+var playGame = function() {
 // prompt humanChoice (use prompt as a function)
 var humanChoice = prompt("Choose r, p, s");
-console.log('human choice',humanChoice);
+console.log("human choice",humanChoice);
 
 // randomly choose computerChoice (use .random)
-var random = math.floor(math.random() * choices.length)
-var computerChoice = choices[random]
-console.log('computer choice', computerChoice)
+var random = math.floor(math.random() * choices.length);
+var computerChoice = choices[random];
+console.log("computer choice", computerChoice);
 
 var result
 // compare choices
 if (humanChoice === computerChoice) {
   // increse ties by 1
   ties++
-  result = "tied!"
+  alert("tied!");
 }
 else if (
   // ( human chose r and the computer chose s )
-  humanChoice === "r" && computerChoice === "s" ||
+  (humanChoice === "r" && computerChoice === "s") ||
   // ( human chose p and the computer chose r )
-  humanChoice === "p" && computerChoice === "r" ||
+  (humanChoice === "p" && computerChoice === "r") ||
   // (human chose s and the computer chose p )
-  humanChoice === "s" && computerChoice === "p" ||
-) {
-  //increse wins by 1
+  (humanChoice === "s" && computerChoice === "p")
+) 
+
+//increse wins by 1
+{
   wins++
-  result = "win!"
+  alert("win!");
 }
 else {
   // increse losses by 1
   losses++
-  result = "lost!"
+  alert("lost!");
 }
 
 // display (alert) comparison results (won, tied, lost round)
 alert ("You " + result)
 // show stats (number of wins, losses, ties)
-alert("Stats:nWins: " + wins + "/nLosses: " + losses + "/nTies " +ties)
+alert(
+  "Stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties
+);
 
 // play again?
-keepPlaying = confirm("Want to play again?")
+var keepPlaying = confirm("Want to play again?")
 // restart game (if yes)
-  // end game (cancel)
+if (playAgain) {
+  playGame();
+}
+};
 
+playGame();
 
 
 
