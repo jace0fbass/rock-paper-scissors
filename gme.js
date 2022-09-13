71d185c486ed614a426abb2e402a86ccde85c536
Jason <jajones414@gmail.com>
2022-09-12 19:38:30 -0500

    I have began laying out the pseudo-code in the script.js file.

--- gme.js
+++ gme.js
@@ -1,1 +1,36 @@
-console.log('yo')
+// Pseudo-coding
+
+
+// define age variable
+// define name variable
+
+// if the age is greater than or equal to 17 (if block condition)
+    // admit them to the movie (pass condition)
+//else (else condition)
+    // deny them entry
+
+// define array to hold choices [r, p, s]
+// define var for computerChoice
+// define var for humanChoice
+// define var for wins
+// define var for losses
+// define var for ties
+
+// prompt humanChoice (use prompt as a function)
+
+// randomly choose computerChoice (use .random)
+
+// compare choices
+   // if humanChoice is p and computer choice is r
+//humanChoice wins
+
+// display (alert) comparison results (won, tied, lost round)
+
+// show stats (number of wins, losses, ties)
+
+
+// play again?
+    // restart game (if yes)
+// else
+    // end game (cancel)
+
